1869a8fefebb921761b846e433414a1c1b26a1c6
Mohamed Mostafa <powermostafa50015@gmail.com>
2021-02-09 20:27:13 +0200

    edit section strac

--- src/script/partials/portfolio.js
+++ src/script/partials/portfolio.js
@@ -1,22 +1,29 @@
-$(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    rtl: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 2,
-      },
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  loop: true,
+  rtl: true,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
     },
-  });
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+  },
 });
+$('.slider-arrow-right').click(function() {
+  owl.trigger('next.owl.carousel');
+  owl.trigger('stop.owl.autoplay');
+});
+$('.slider-arrow-left').click(function() {
+  owl.trigger('prev.owl.carousel');
+  owl.trigger('stop.owl.autoplay');
+})
