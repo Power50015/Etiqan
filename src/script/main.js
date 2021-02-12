83ea082d14bee503697e56fe23fc4657747b1e70
Mohamed Mostafa <powermostafa50015@gmail.com>
2021-02-12 14:46:26 +0200

    fix whatsapp btn in galaxy note | hotfix

--- src/script/main.js
+++ src/script/main.js
@@ -10,4 +10,9 @@ import "./partials/about";
 import "./partials/scroll";
 import "./partials/portfolio";
 
-
+$(".fixed-btn").on("click", function (e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log("you clicked on button #");
+  window.open("https://api.whatsapp.com/send?phone=+966550561364&text=Hello", "_blank");
+});
--- src/script/partials/scroll.js
+++ src/script/partials/scroll.js
@@ -3,7 +3,7 @@ $("html").niceScroll({
   cursoropacitymin: 1, // change opacity when cursor is inactive (scrollabar "hidden" state), range from 1 to 0
   cursorborder: "none", // css definition for cursor border
   cursorborderradius: "1px", // border radius in pixel for cursor
-  zindex: "999999", // change z-index for scrollbar div
+  zindex: "999", // change z-index for scrollbar div
   scrollspeed: 10, // scrolling speed
   mousescrollstep: 10, // scrolling speed with mouse wheel (pixel)
   horizrailenabled: false,
