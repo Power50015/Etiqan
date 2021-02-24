16a62e06eed37c0252b9e76903a1dbd05a9698dd
Mohamed Mostafa <powermostafa50015@gmail.com>
2021-02-24 10:03:41 +0200

    add msg to whatsapp btn

--- src/script/partials/whatsapp.js
+++ src/script/partials/whatsapp.js
@@ -1,6 +1,22 @@
 $(".fixed-btn").on("click", function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    console.log("you clicked on button #");
-    window.open("https://api.whatsapp.com/send?phone=+966550561364&text=Hello", "_blank");
-  });
+  e.stopPropagation();
+  e.preventDefault();
+  console.log("you clicked on button #");
+  window.open(
+    "https://api.whatsapp.com/send?phone=+966550561364&text=Hello",
+    "_blank"
+  );
+});
+var falg = true;
+$(document, window).scroll(function () {
+  if (falg) {
+    if ($(document).scrollTop() > 900) {
+      falg = false;
+      $(".whatsapp-msg").animate({ bottom: "28px" }, 200);
+    }
+  }
+});
+
+$(".fixed-btn").click(function () {
+  $(".whatsapp-msg").hide();
+});
