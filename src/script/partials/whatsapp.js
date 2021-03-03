20e70d0e282f1f38f7b4e72e6799f0382e58b67e
Mohamed Mostafa <powermostafa50015@gmail.com>
2021-03-03 20:30:30 +0200

    fix some google analsis issue

--- src/script/partials/whatsapp.js
+++ src/script/partials/whatsapp.js
@@ -1,9 +1,8 @@
 $(".fixed-btn").on("click", function (e) {
   e.stopPropagation();
   e.preventDefault();
-  console.log("you clicked on button #");
   window.open(
-    "https://api.whatsapp.com/send?phone=+966550561364&text=Hello",
+    "https://api.whatsapp.com/send?phone=+966550561364&text=السلام عليكم",
     "_blank"
   );
 });
